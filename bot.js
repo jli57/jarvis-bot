2fb5cc28b7d748ecd31e4ebc627d999d98944482
Jingna <jingna13@gmail.com>
2018-08-14 17:57:10 -0700

    Basic Jarvis Impelemntation complete

--- bot.js
+++ bot.js
@@ -1,39 +1,45 @@
-var Discord = require('discord.io');
-var logger = require('winston');
-var auth = require('./auth.json');
-// Configure logger settings
-logger.remove(logger.transports.Console);
-logger.add(logger.transports.Console, {
-    colorize: true
-});
-logger.level = 'debug';
-// Initialize Discord Bot
-var bot = new Discord.Client({
-   token: auth.token,
-   autorun: true
-});
-bot.on('ready', function (evt) {
-    logger.info('Connected');
-    logger.info('Logged in as: ');
-    logger.info(bot.username + ' - (' + bot.id + ')');
-});
-bot.on('message', function (user, userID, channelID, message, evt) {
-    // Our bot needs to know if it will execute a command
-    // It will listen for messages that will start with `!`
-    if (message.substring(0, 1) == '!') {
-        var args = message.substring(1).split(' ');
-        var cmd = args[0];
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const config = require("./config.json");
 
-        args = args.splice(1);
-        switch(cmd) {
-            // !ping
-            case 'ping':
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'Pong!'
-                });
-            break;
-            // Just add any case commands if you want to..
-         }
-     }
+client.on("ready", () => {
+  console.log("I am ready!");
 });
+
+const prefix = config.prefix;
+client.on("message", (message) => {
+  // Exit and stop if it's not there
+  if (message.author.bot) return;
+  if (message.content.indexOf(config.prefix) !== 0) return;
+
+  const args = message.content.slice(prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+
+  if(command === 'ping') {
+    message.channel.send('Pong!');
+  } else
+  if (command ===  "foo" ) {
+    message.channel.send("bar!");
+  } else
+  if (command === "asl") {
+    let [age, sex, location] = args;
+    message.reply(`Hello ${message.author.username}, I see you're a ${age} year old ${sex} from ${location}. I'm a cute anime grill, wanna date?`);
+  } else
+  if (command === "jing" ) {
+    message.channel.send("My master is away now <@" + config.ownerID+ ">");
+  } else
+  if (command === "crespo" ) {
+    message.channel.send("<@223990026608967680>");
+  } else
+  if (command === "nunu" ) {
+    message.channel.send("<@256605450529800192>");
+  } else
+  if (command === "mexicofrenzy" ) {
+    message.channel.send("<@132964999327907842>");
+  } else
+  if (command === "tsundere" ) {
+    message.channel.send("<@158731791400697856>");
+  }
+});
+
+client.login(config.token)
