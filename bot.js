5e416914d1e48b2b008de25eb952d982408264b0
Jingna <jingna13@gmail.com>
2018-08-15 12:59:57 -0700

    remove user specific commands

--- bot.js
+++ bot.js
@@ -22,24 +22,11 @@ client.on("message", message => {
   const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
 
-  if (command === "crespo" ) {
-    message.channel.send("<@223990026608967680>");
-  } else
-  if (command === "nunu" ) {
-    message.channel.send("<@256605450529800192>");
-  } else
-  if (command === "mexicofrenzy" ) {
-    message.channel.send("<@132964999327907842>");
-  } else
-  if (command === "tsundere" ) {
-    message.channel.send("<@158731791400697856>");
-  } else {
-    try {
-      let commandFile = require(`./commands/${command}.js`);
-      commandFile.run(client, message, args, config.ownerID.toString());
-    } catch (err) {
-      console.error(err);
-    }
+  try {
+    let commandFile = require(`./commands/${command}.js`);
+    commandFile.run(client, message, args, config.ownerID.toString());
+  } catch (err) {
+    console.error(err);
   }
 });
 
