02950f2cd81685b8a36df3473cf4aaa22fe63ffc
Jingna <jingna13@gmail.com>
2018-08-15 13:17:46 -0700

    change config file

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 const fs = require("fs");
 
-const config = require("./config-test.json");
+const config = require("./config.json");
 const prefix = config.prefix;
 
 // add events
