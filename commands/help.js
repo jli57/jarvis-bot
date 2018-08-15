aa7ef0944d649502ebf6f5b256cf93c6ac03342f
Jingna <jingna13@gmail.com>
2018-08-15 10:49:18 -0700

    added help command

---
+++ commands/help.js
@@ -0,0 +1,9 @@
+const fs = require("fs");
+
+exports.run = (client, message, args) => {
+  fs.readdir("./commands/", (err,files) => {
+    if (err) return console.error(err);
+    let commandNames = files.map(file => file.split(".")[0] ).join(", ");
+    message.channel.send(`My commands are: ${commandNames}`);
+  });
+}
