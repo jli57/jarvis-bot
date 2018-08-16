0775095f6f15de3652fcfc27e2a140af5e15eb34
Jingna <jingna13@gmail.com>
2018-08-16 11:01:57 -0700

    Revert "update .gitignore"
    This reverts commit 66ce65ee8df436ed74b8afbd70026a938ac74385.

--- commands/jing.js
+++
@@ -1,3 +0,0 @@
-exports.run = (client, message, args, config) => {
-  message.channel.send(`My master, <@${config.ownerID}>, might be away right now.`);
-}
--- src/commands/asl.js
+++ src/commands/asl.js
@@ -1,7 +1,4 @@
 exports.run = (client, message, args) => {
   let [age, sex, location] = args;
-  if (args.length < 3) {
-
-  }
   message.channel.send(`Hi ${message.author}, I see you are a ${age} year old ${sex} from ${location}. I'm a cute anime grill, wanna date?`).catch(console.error);
 }
