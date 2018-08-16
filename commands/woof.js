2bc3dbccea94d07760ac0670838e74587ad8a2db
Jingna <jingna13@gmail.com>
2018-08-16 11:33:19 -0700

    delete extra command folder

--- commands/woof.js
+++
@@ -1,8 +0,0 @@
-exports.run = (client, message, args) => {
-  message.channel.send({
-    files: [{
-      attachment: "./images/doge.jpg",
-      name: "dog.jpg"
-    }]
-  }).catch(console.error);
-}
